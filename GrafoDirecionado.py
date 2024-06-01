613852179037e9ae65deb525c84e2e3d412cbcc7
izabelly-code <izabelly.mucholowskiribeiro@gmail.com>
2024-06-01 10:03:04 -0300

    Grafo direcionado

---
+++ GrafoDirecionado.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+class GrafoDirecionado:
+    def __init__(self):
+        self.lista_adjacencias = defaultdict(list)
+        self.num_vertices = 0
+        self.num_arestas = 0
+
+    def adiciona_vertice(self, u):
+        if u not in self.lista_adjacencias:
+            self.lista_adjacencias[u] = []
+            self.num_vertices += 1
+
+    def adiciona_aresta(self, u, v):
+        if v not in self.lista_adjacencias:
+            self.adiciona_vertice(v)
+        if u not in self.lista_adjacencias:
+            self.adiciona_vertice(u)
+        if not any(aresta for aresta in self.lista_adjacencias[u] if aresta[0] == v):
+            self.lista_adjacencias[u].append((v, 1))
+            self.num_arestas += 1
+                
+        else:
+            for to in self.lista_adjacencias[u]:
+                if to[0] == v:
+                    segundo_valor = to[1]
+                    novo_segundo_valor = segundo_valor + 1  
+                    nova_tupla = (to[0], novo_segundo_valor)
+                    indice = self.lista_adjacencias[u].index(to)
+                    self.lista_adjacencias[u][indice] = nova_tupla
--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-import pandas as pd
-import unicodedata
+import csv
+import sys
+from GrafoDirecionado import GrafoDirecionado;
+
+sys.stdin.reconfigure(encoding='utf-8')
+sys.stdout.reconfigure(encoding='utf-8')
 
 
-# Example usage
+grafoDirecionado = GrafoDirecionado()
 file_path = "netflix_amazon_disney_titles.csv"
-data = pd.read_csv(file_path)
+try:
+    with open(file_path, 'r', encoding='utf-8',errors='ignore') as f:
+        reader = csv.reader(f)
+        next(reader)
+        for linha in reader:
+            # CONSTRUÇÃO DO GRAFO DIRECIONADO ENTRE DIRETOR E ATORES
+            if linha[3] != "" and linha[4] != "":
+                diretor=linha[3]
+                cast=linha[4].split(",")
+                for ator in cast:
+                    grafoDirecionado.adiciona_aresta(diretor, ator)
 
-fifth_column = data.iloc[:, 4]
-split_data = fifth_column.str.split(',')
-
-for index, row in split_data.items():
-    for item in row:
-        item=unicodedata.normalize('NFD', item).encode('ascii', 'ignore')
-        print(item)
+except Exception as e:
+    print(f"An error occurred: {e}")
